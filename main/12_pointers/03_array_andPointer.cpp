4b97d5246a15ff071df86ad7c22388043e744847
Balaji <balajimail@zoho.com>
2021-09-30 08:17:32 +0530

    wel

--- main/12_pointers/03_array_andPointer.cpp
+++ main/12_pointers/03_array_andPointer.cpp
@@ -6,6 +6,17 @@ int main(void){
     cout<<*arr<<endl;//1
     int *arr_ptr = arr;
     cout<<arr_ptr<<endl;//Address
+    cout<<(arr_ptr+1)<<endl;//arr ptr is increased by 4 not 1
     cout<<*arr_ptr<<endl;//1
+    cout<<arr_ptr[2]<<endl;//Indexing via Pointer
+    cout<<*(arr_ptr+2)<<endl;
     return 0;
 }
+/**
+ * 
+ * Subscript Notation   |   Offset Notation
+ * ---------------------------------------------
+ * array_name[index]    |  *(array_name + index)
+ * pointer_name[index]  |  *(pointer_name + index)
+ * 
+*/
